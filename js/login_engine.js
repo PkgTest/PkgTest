ba9f3a1f21704fa41199c57b08943685bdd7fa2b
dev1 <r2d2isajedi@gmail.com>
2021-08-08 12:34:57 +0800

    initialcommit

---
+++ js/login_engine.js
@@ -0,0 +1,19 @@
+/*
+This script manages login process.
+*/
+
+function validateLogin(){
+    let email=document.getElementById("email").nodeValue;
+    let password=document.getElementById("password").nodeValue;
+}
+
+function Pwd_display(){
+    let img = document.getElementById("show_pwd").style.backgroundImage;
+    if (img.includes("crossed_eye.png")){
+        document.getElementById("show_pwd").style.backgroundImage = "url(../img/eye.png)";
+        document.getElementById("password").type = "text";
+    } else {
+        document.getElementById("show_pwd").style.backgroundImage = "url(../img/crossed_eye.png)";
+        document.getElementById("password").type = "password";
+    }
+}
